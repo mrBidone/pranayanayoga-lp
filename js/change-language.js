646cdfa5f1b60e03b172ecff6ab78f09e46ea889
mrBidone <ab.bz@ukr.net>
2024-10-05 19:22:56 +0200

    Fix js-title

--- js/change-language.js
+++ js/change-language.js
@@ -55,9 +55,9 @@ function setInitialLanguage(translations) {
 
 function applyTranslation(lang, translations) {
   // Изменяем заголовок HTML страницы
-  // document.title =
-  //   translations[lang].title ||
-  //   "Yoga lessons in Barcelona with Yana Selitskaya"; // Резервный заголовок на случай отсутствия данных
+  document.title =
+    translations[lang].title ||
+    "Yoga lessons in Barcelona with Yana Selitskaya"; // Резервный заголовок на случай отсутствия данных
 
   // Обновляем текстовые элементы на странице
   Object.keys(elements).forEach((key) => {
